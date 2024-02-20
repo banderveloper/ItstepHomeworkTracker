edaad4eedb8c3e8b9a56c09914f62e77cebbd603
banderveloper <kpssdeveloper@gmail.com>
2024-02-20 20:18:33 +0200

    refactor: add entities

---
+++ src/Library/Entities/Homework.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ItstepHomeworkTracker.Library.Entities;
+
+internal class Homework
+{
+    [JsonPropertyName("isCompleted")]
+    public bool IsCompleted { get; set; }
+    [JsonPropertyName("grade")]
+    public int? Grade { get; set; }
+}
