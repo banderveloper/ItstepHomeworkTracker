b2028b286117a7e0673d57e86a87947732cea74b
banderveloper <kpssdeveloper@gmail.com>
2024-02-20 20:10:14 +0200

    refactor: create classlib project, implement core presentation library class

---
+++ src/Library/HomeworkTrackerEventArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace ItstepHomeworkTracker.Library;
+
+public class HomeworkTrackerEventArgs: EventArgs
+{
+    public string Message { get; }
+
+    public HomeworkTrackerEventArgs(string message) => Message = message;
+}
---
+++ src/Library/LogbookWebDriver.cs
@@ -0,0 +1,15 @@
+﻿namespace ItstepHomeworkTracker.Library;
+
+internal class LogbookWebDriver
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string GroupName { get; set; }
+    public int TotalHomeworksCount { get; set; }
+    public int RequiredHomeworksPercent { get; set; }
+
+    public void Start()
+    {
+
+    }
+}
