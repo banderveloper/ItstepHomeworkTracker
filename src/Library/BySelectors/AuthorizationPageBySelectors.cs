b05332f04323dcd08b46b110a1c2aa1cf4d88d9b
banderveloper <kpssdeveloper@gmail.com>
2024-02-20 20:31:24 +0200

    refactor: add authorization page selectors and start authorize method

---
+++ src/Library/BySelectors/AuthorizationPageBySelectors.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+
+namespace ItstepHomeworkTracker.Library.BySelectors;
+
+/// <summary>
+/// Selenium By selectors for authorization page
+/// </summary>
+internal static class AuthorizationPageBySelectors
+{
+    /// <summary>
+    /// Login input
+    /// </summary>
+    public static readonly By LoginInput = By.Id("login");
+
+    /// <summary>
+    /// Password input
+    /// </summary>
+    public static readonly By PasswordInput = By.Id("password");
+
+    /// <summary>
+    /// Submit button
+    /// </summary>
+    public static readonly By SubmitButton = By.ClassName("btn-login");
+}
