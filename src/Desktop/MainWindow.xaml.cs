a093e3c4e031f9b4e44f7bdf0606ee7f46fd84e4
banderveloper <kpssdeveloper@gmail.com>
2024-02-22 01:25:44 +0200

    feat: implement main window markup

--- src/Desktop/MainWindow.xaml.cs
+++ src/Desktop/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,5 +20,16 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnExitButtonClick(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void OnNumberTextBoxValidate(object sender, TextCompositionEventArgs e)
+    {
+        var regex = new Regex("[^0-9]+");
+        e.Handled = regex.IsMatch(e.Text);
     }
 }
