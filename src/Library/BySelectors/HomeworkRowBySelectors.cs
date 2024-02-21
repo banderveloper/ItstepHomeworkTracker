0b729042e4333a3f4ffd1e774fc7ac0ee9be4cd8
banderveloper <kpssdeveloper@gmail.com>
2024-02-21 03:34:33 +0200

    refactor: implement method for counting count of homeworks pages

---
+++ src/Library/BySelectors/HomeworkRowBySelectors.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace ItstepHomeworkTracker.Library.BySelectors;
+
+internal static class HomeworkRowBySelectors
+{
+    public static By StudentNameElement = By.CssSelector("td[class='student-name'] p");
+    public static By HomeworkItem = By.ClassName("hw_selects");
+
+    public static By CompletedHomeworkItem = By.ClassName("hw_checked");
+    public static By NewHomeworkItem = By.ClassName("hw_new");
+    public static By UnloadedHomeworkItem = By.ClassName("hw_not-loaded");
+}
