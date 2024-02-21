85a82c88634b2b98e8c3660208cf9cde15d103b8
banderveloper <kpssdeveloper@gmail.com>
2024-02-21 02:18:52 +0200

    refactor: implement throwing error cause of failed authorization

---
+++ src/Library/Exceptions/TimeoutException.cs
@@ -0,0 +1,9 @@
+﻿namespace ItstepHomeworkTracker.Library.Exceptions;
+
+/// <summary>
+/// Exception, thrown after finding time out
+/// </summary>
+internal class TimeoutException : Exception
+{
+    public TimeoutException(string message) : base(message) {}
+}
