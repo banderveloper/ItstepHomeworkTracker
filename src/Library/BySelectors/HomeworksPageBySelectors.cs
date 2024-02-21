ab0548eb2589695792d4584bdc7daa43d1a3e28b
banderveloper <kpssdeveloper@gmail.com>
2024-02-21 02:52:17 +0200

    refactor: add waiting methods, and going to homeworks page with closing popup

---
+++ src/Library/BySelectors/HomeworksPageBySelectors.cs
@@ -0,0 +1,8 @@
+﻿using OpenQA.Selenium;
+
+namespace ItstepHomeworkTracker.Library.BySelectors;
+
+internal static class HomeworksPageBySelectors
+{
+    public static By NewHomeworksPopupCloseButton = By.ClassName("hw-md__close");
+}
