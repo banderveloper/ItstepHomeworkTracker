a2b3f91c2ebe097166c98707ef494c6619d4b55d
banderveloper <kpssdeveloper@gmail.com>
2024-02-22 02:45:10 +0200

    feat: implement highlighting empty fields on start click

---
+++ src/Desktop/Extensions/DependencyObjectExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ItstepHomeworkTracker.Desktop.Extensions;
+
+public static class DependencyObjectExtensions
+{
+    public static IEnumerable<T> FindVisualChildren<T>(this DependencyObject? dependencyObject) where T : DependencyObject
+    {
+        if (dependencyObject is null)
+        {
+            yield return (T)Enumerable.Empty<T>();
+            yield break;
+        }
+        
+        for (var i = 0; i < VisualTreeHelper.GetChildrenCount(dependencyObject); i++)
+        {
+            var ithChild = VisualTreeHelper.GetChild(dependencyObject, i);
+            if (ithChild is T t) yield return t;
+            foreach (var childOfChild in FindVisualChildren<T>(ithChild)) yield return childOfChild;
+        }
+    }
+}
