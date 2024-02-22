a5c91ef4c76c1aaa20d6edd924de0c9eaf3c7fef
banderveloper <kpssdeveloper@gmail.com>
2024-02-22 02:45:10 +0200

    feat: implement highlighting empty fields on start click

--- src/Desktop/MainWindow.xaml.cs
+++ src/Desktop/MainWindow.xaml.cs
@@ -1,7 +1,14 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using ItstepHomeworkTracker.Desktop.Extensions;
 using ItstepHomeworkTracker.Library;
+using MaterialDesignColors;
 
 namespace ItstepHomeworkTracker.Desktop;
 
@@ -11,10 +18,17 @@ namespace ItstepHomeworkTracker.Desktop;
 public partial class MainWindow : Window
 {
     private HomeworkTracker _tracker;
+    private Brush _defaultTextBoxBrush;
 
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnLoad(object sender, RoutedEventArgs e)
+    {
+        _defaultTextBoxBrush = UsernameTextBox.BorderBrush;
+        RequiredHomeworksPercentTextBox.Text = "80";
     }
 
     private void OnExitButtonClick(object sender, RoutedEventArgs e)
@@ -24,6 +38,12 @@ public partial class MainWindow : Window
 
     private void OnStartButtonClick(object sender, RoutedEventArgs e)
     {
+        if (!IsTextBoxesFilled())
+        {
+            HighlightEmptyTextboxes();
+            return;
+        }
+
         _tracker = new HomeworkTracker()
             .AddCredentials(UsernameTextBox.Text, PasswordTextBox.Password)
             .AddTargetGroupName(GroupNameTextBox.Text)
@@ -42,6 +62,18 @@ public partial class MainWindow : Window
         e.Handled = regex.IsMatch(e.Text);
     }
 
+    private void OnTextBoxTextChange(object sender, TextChangedEventArgs e)
+    {
+        var textBox = sender as TextBox;
+        textBox.BorderBrush = textBox.Text.Length > 0 ? _defaultTextBoxBrush : Brushes.IndianRed;
+    }
+
+    private void OnPasswordBoxChange(object sender, RoutedEventArgs e)
+    {
+        var passwordBox = sender as PasswordBox;
+        passwordBox.BorderBrush = passwordBox.Password.Length > 0 ? _defaultTextBoxBrush : Brushes.IndianRed;
+    }
+
     private void OnTrackerError(object sender, HomeworkTrackerEventArgs e)
     {
         MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
@@ -50,6 +82,23 @@ public partial class MainWindow : Window
     private void OnTrackerFinish(object sender, HomeworkTrackerEventArgs e)
     {
         MessageBox.Show("Done!");
+    }
+
+    private bool IsTextBoxesFilled()
+    {
+        return
+            UsernameTextBox.Text.Length * PasswordTextBox.Password.Length *
+            GroupNameTextBox.Text.Length * HomeworksCountTextBox.Text.Length *
+            RequiredHomeworksPercentTextBox.Text.Length > 0;
+    }
+
+    private void HighlightEmptyTextboxes()
+    {
+        var textBoxes = this.FindVisualChildren<TextBox>().Where(tb => tb.Text.Length == 0);
+        foreach (var textBox in textBoxes) textBox.BorderBrush = Brushes.IndianRed;
+        
+        var passwordBox = this.FindVisualChildren<PasswordBox>().FirstOrDefault(pb => pb.Password.Length == 0);
+        if (passwordBox != null) passwordBox.BorderBrush = Brushes.IndianRed;
     }
 
     protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
